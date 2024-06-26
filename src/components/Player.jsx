3aa9bc873311f5199ab5cba1209ca915cabb0a9e
Seán Hester <sean.hester@zinkworks.com>
2024-06-26 14:12:45 +0100

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function Player({name, symbol}) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleClick(){
+        setIsEditing(true);
+    }
+
+    let playerName = <span className='player-name'>{name}</span>;
+    // let btnCaption = 'Edit';
+
+    if(isEditing){
+        playerName = <input type="text" required value={name} />;
+    }
+
+    return (
+        <li>
+          <span className="player">
+            {playerName}
+            <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleClick}>{isEditing ? 'Save': 'Edit'}</button>
+        </li>
+    )
+}
